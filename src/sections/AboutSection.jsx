5cdcbb6c44a1da54034a8d29e6720ca1f1d2a05a
Nikkoro <90435784+id-Nick010@users.noreply.github.com>
2025-07-09 21:53:59 +0800

    feat(about): create base and primary styling of about section

--- src/sections/AboutSection.jsx
+++ src/sections/AboutSection.jsx
@@ -4,8 +4,39 @@ import '../styles/AboutSection.css';
 function AboutSection() {
     return(
         <section className='aboutMeRoot'>
-            <p>this is about me</p>
+            <div className="tint"></div>
+            <section className='sectionCenter'>
+                <main className='MainAboutMe'>
+                    <h1>ABOUT<span>ME</span></h1>
+                    <h2>deve/oper</h2>
+                    <p>
+                        I’m a full-stack developer driven by innovation, Artistry, and efficiency. 
+                        I craft seamless digital experiences that blend elegant design with robust functionality. 
+                        While I specialize in frontend and backend development, I also actively explore AI and machine learning, fueling my curiosity and expanding what I can build.
+                    </p>
+                </main>
+                <aside className='asideBox'>
+                    <h2>DEVXFRONTEND</h2>
+                    <h2>BACKEND</h2>
+                    <h2>AI/ML</h2>
+                    <h2>WEB</h2>
+                    <h2>MOBILE</h2>
+                </aside>
+            </section>
+            <div className='divider'></div>
+            <footer className='footerEducation'>
+                <h4>Education</h4>
+                <section>
+                    <h4>BS Computer Science</h4>
+                    <p>Bicol University</p>
+                </section>
+                <section>
+                    <h4>NC2 Java Programming</h4>
+                    <p>Tesda NRG</p>
+                </section>
+            </footer>
         </section>
+        
     );
 }
 
