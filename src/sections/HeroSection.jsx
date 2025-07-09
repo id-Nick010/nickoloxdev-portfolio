c543d16b9ff79bf828de544b1ce89e7cd4061f04
Nikkoro <90435784+id-Nick010@users.noreply.github.com>
2025-07-09 20:38:27 +0800

    feat(hero): add and change the font of main text header & nav

--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import '../styles/HeroSection.css'; 
-import Silk from '../components/design/Silk';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAnglesDown, faSquareEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
